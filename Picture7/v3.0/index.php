9c8b0ac4c54895d49f5168caff4ccd4f2c0ac97e
makandat <tadnakam@outlook.jp>
2019-01-01 12:03:16 +0900

    Picture7 v3.40 -> 3.50

--- Picture7/v3.0/index.php
+++ Picture7/v3.0/index.php
@@ -2,30 +2,36 @@
 include "OOLib1.php";
 include "MySQL.php";
 
-// まんが作者一覧
+// 画像一覧クラス
 class Pictures extends OOPHPLib\WebPage {
 
   // コンストラクタ
   public function __construct() {
     parent::__construct("index.html");
     // 条件指定があるか？
-    if (isset($_GET['FAV'])) {
-        $this->v['table'] = $this->listCreators("FAV <> '0'");
+    if (isset($_GET['filterPath'])) {
+      $this->v['table'] = $this->listManga("path LIKE '%" . $_GET['filterPath'] . "%'");
+    }
+    else if (isset($_GET['FAV'])) {
+        $this->v['table'] = $this->listManga("FAV <> '0'");
     }
     else if (isset($_GET['mark'])) {
-        $this->v['table'] = $this->listCreators("mark='".$_GET['mark']."'");
+        $this->v['table'] = $this->listManga("mark='".$_GET['mark']."'");
+    }
+    else if (isset($_GET['info'])) {
+      $this->v['table'] = $this->listManga("info LIKE '%" . $_GET['info'] . "%'");
     }
     else if ($_GET['creator'] == "") {
-      $this->v['table'] = $this->listCreators();
+      $this->v['table'] = $this->listManga();
     }
     else {
-      $this->v['table'] = $this->listCreators("CREATOR='".$_GET['creator']."'");
+      $this->v['table'] = $this->listManga("CREATOR='".$_GET['creator']."'");
     }
     $this->v['message'] = "";
   }
 
-  // まんが作者一覧を取得する。
-  private function listCreators($criteria = null) {
+  // まんが一覧を取得する。
+  private function listManga($criteria = null) {
     $strbuff = "";
     $conn = new OOPHPLib\MySQL('AppConf.ini');
     $sql = "SELECT * FROM Pictures";
@@ -33,7 +39,6 @@ class Pictures extends OOPHPLib\WebPage {
       $sql .= " WHERE " . $criteria;
     }
     $sql .= " ORDER BY id DESC LIMIT 500";
-    #print $sql;
     $dataTable = $conn->query($sql);
     if (count($dataTable) > 0) {
       $strbuff .= "<table style='width:100%;'>\n<tr><th>ID</th><th>タイトル</th><th>作者</th><th>種別</th><th>備考</th>".
