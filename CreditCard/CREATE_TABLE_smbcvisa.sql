7f1cb283468b0a58c0a187f54b5dd1ab37386c66
makandat@japan <tadnakam@outlook.jp>
2019-01-15 17:08:45 +0900

    Delete CREATE_TABLE_smbcvisa.sql
    Sync

--- CreditCard/CREATE_TABLE_smbcvisa.sql
+++
@@ -1,7 +0,0 @@
--- 三井住友ビザカード支払い管理
-CREATE TABLE `smbcvisa` (
-  `date` char(10) NOT NULL,
-  `payment` decimal(10,0) NOT NULL,
-  `info` varchar(128) DEFAULT NULL,
-  PRIMARY KEY (`date`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
