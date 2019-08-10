d7b6a3d0cd8c566d06acb45e393f72de5fe012dd
makandat <tadnakam@outlook.jp>
2019-08-10 16:41:59 +0900

    listPics.py 機能追加

--- Pictures(PYTHON3)/tools/listPics.py
+++ Pictures(PYTHON3)/tools/listPics.py
@@ -2,12 +2,13 @@
 from Py365Lib import Common, FileSystem as fs
 import pathlib
 import os
+import re
 
 # 画像拡張子一覧
 ImgExts = ['.jpg', '.jpeg', '.png', '.gif']
 
 # Pictures テーブルのフィールド
-PTF = "INSERT INTO Pictures VALUES(NULL, '{0}', '作者', '{1}', '', '', 0, 0, 0);"
+PTF = "INSERT INTO Pictures VALUES(NULL, '{0}', '{1}', '{2}', '', '', 0, 0, 0);"
 
 # パラメータチェック
 if Common.count_args() == 0 :
@@ -35,7 +36,12 @@ for dir in dirset :
   parts = str(dir).split("/")
   n = len(parts) - 1
   title = parts[n]
+  m = re.search(r"\[.+\]", title)
+  creator = "作者"
+  if m :
+    s = m.group(0)
+    creator = s[1:len(s) - 1]
   # SQL を表示 (リダイレクトを使ってファイル保存)
-  print(PTF.format(title, dir))
+  print(PTF.format(title, creator, dir))
 
 # print("終わり")
