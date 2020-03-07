d7e76a98bc9d3836a217dc581c9c66455cb1d5b6
makandat <tadnakam@outlook.jp>
2020-03-07 12:27:01 +0900

    SQL, DAteTime.py 追加

--- PictureAlbum/CREATE_TABLE_PictureAlbum.sql
+++ PictureAlbum/CREATE_TABLE_PictureAlbum.sql
@@ -1,21 +1,29 @@
-CREATE TABLE Album (
-  id int not null auto_increment,
-  name varchar(50) not null,
-  mark varchar(10),
-  info varchar(100),
-  bindata int default 0,
-  primary key(id)
-);
+CREATE TABLE `album` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `mark` varchar(10) DEFAULT NULL,
+  `info` varchar(100) DEFAULT NULL,
+  `bindata` int(11) DEFAULT '0',
+  `groupname` varchar(30) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `album_name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=154 DEFAULT CHARSET=utf8;
 
-
-CREATE TABLE PictureAlbum (
-  id int(11) not null auto_increment,
-  album int not null,
-  title varchar(100) not null,
-  path varchar(500) not null,
-  creator varchar(100) not null,
-  info varchar(100),
-  fav int(1) default 0,
-  bindata int(11) default 0,
-  primary key(id)
-);
+CREATE TABLE `picturealbum` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `album` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `path` varchar(500) NOT NULL,
+  `creator` varchar(100) NOT NULL,
+  `info` varchar(100) DEFAULT NULL,
+  `fav` int(1) DEFAULT '0',
+  `bindata` int(11) DEFAULT '0',
+  `picturesid` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `path` (`path`),
+  KEY `album_path` (`path`),
+  KEY `album_number` (`album`)
+) ENGINE=InnoDB AUTO_INCREMENT=10835 DEFAULT CHARSET=utf8;
--- Pictures(PYTHON3)/CREATE_TABLE_Pictures.sql
+++ Pictures(PYTHON3)/CREATE_TABLE_Pictures.sql
@@ -1,14 +1,19 @@
--- 画像ファイル管理
 CREATE TABLE `Pictures` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `TITLE` varchar(100) NOT NULL,
-  `CREATOR` varchar(50) NOT NULL,
-  `PATH` varchar(500) NOT NULL UNIQUE,
+  `CREATOR` varchar(100) NOT NULL,
+  `PATH` varchar(500) NOT NULL,
   `MARK` varchar(10) DEFAULT NULL,
   `INFO` varchar(100) DEFAULT NULL,
-  `FAV` char(1) DEFAULT '0',
-  `COUNT` int(8) DEFAULT 0,
-  `BINDATA` int(11) DEFAULT 0,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=279 DEFAULT CHARSET=utf8;
-
+  `fav` int(4) DEFAULT '0',
+  `COUNT` int(8) DEFAULT '0',
+  `BINDATA` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `idxTitle` (`TITLE`),
+  KEY `idxCreator` (`CREATOR`),
+  KEY `idxInfo` (`INFO`),
+  KEY `idxPath` (`PATH`),
+  KEY `idx_info` (`INFO`),
+  KEY `idx_mark` (`MARK`)
+) ENGINE=InnoDB AUTO_INCREMENT=17342 DEFAULT CHARSET=utf8;
