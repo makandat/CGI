214014422b4acfca3763404297249c226418269c
makandat@japan <tadnakam@hotmail.com>
2023-02-06 09:55:47 +0900

    html.py v1.2
    Bootstrap5, Vue3 にしました。

--- python3_bin/html.py
+++ python3_bin/html.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
 
-#  HTML ファイルのひな型を作成する。 v1.1
+#  HTML ファイルのひな型を作成する。 v1.2
 from Py365Lib import Common, FileSystem as fs
 
 HTML_Simple = '''<!DOCTYPE html>
-<html>
+<html lang="ja">
 <head>
  <meta charset="utf-8" />
- <title>(*title*)</title>
+ <meta name="viewport" content="width=device-width,initial-scale=1" />
+ <title>HTML_Simple</title>
  <link rel="stylesheet" href="/css/style.css" />
- <style></style>
- </head>
+ <style>
+   h1 {
+     text-align: center;
+     color: crimson;
+     padding: 10px;
+   }
+   a:link, a:visited {
+     text-decoration: none;
+     color: firebrick;
+   }
+ </style>
+</head>
 
 <body>
  <!-- ヘッダー -->
  <header>
-  <h1>title</h1>
-  <div class="menubar"><a href="/">HOME</a>&nbsp;/&nbsp;<a href="javascript:window.close()">閉じる</a></div>
+  <h1>HTML_Simple</h1>
  </header>
- 
+
  <!-- 本文 -->
  <article>
    <section>
@@ -37,204 +47,101 @@ HTML_Simple = '''<!DOCTYPE html>
 </html>
 '''
 
-HTML_Highlight = '''<!DOCTYPE html>
-<html>
+HTML_Bootstrap5 = '''<!DOCTYPE html>
+<html lang="ja">
 <head>
  <meta charset="utf-8" />
- <title>(*title*)</title>
- <link rel="stylesheet" href="/css/style.css" />
- <script src="/js/jquery.min.js"></script>
- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.0/styles/vs.min.css">
- <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.0/highlight.min.js"></script>
- <script>hljs.initHighlightingOnLoad();</script>
- <style></style>
- <script>
-   $(() => {
-
-   });
- </script>
-</head>
-
-<body>
- <!-- ヘッダー -->
- <header>
-  <h1>(*title*)</h1>
-  <div class="menubar"><a href="/">HOME</a>&nbsp;/&nbsp;<a href="javascript:window.close()">閉じる</a></div>
- </header>
- 
- <!-- 本文 -->
- <article>
-   <section>
-
-   </section>
- </article>
-
- <!-- フッター -->
- <p>&nbsp;</p>
- <p style="text-align:center;"><a href="#top">TOP</a></p>
- <p>&nbsp;</p>
- <p>&nbsp;</p>
-</body>
-</html>
-'''
-
-HTML_Bootstrap4 = '''<!DOCTYPE html>
-<html>
-<head>
- <meta charset="utf-8" />
- <meta http-equiv="X-UA-Compatible" content="IE=edge">
- <meta name="viewport" content="width=device-width, initial-scale=1">
- <title>(*title*)</title>
- <link rel="stylesheet" href="/css/style2.css" />
+ <meta name="viewport" content="width=device-width,initial-scale=1" />
+ <title>HTML_Bootstrap5</title>
  <!-- BootstrapのCSS読み込み -->
- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
- <link rel="stylesheet" href="/css/style2.css">
- <style></style>
- <script>
-   $(() => {
-
-   });
- </script>
-</head>
-
-<body>
-  <!-- ヘッダー -->
-  <header class="container" style="padding:24px;">
-   <h1 id="header">(*title*)</h1>
-   <h5>Updated on (*date*)</h5>
-   <hr />
-  </header>
-
-  <!-- Bootstrap コンテナ -->
-  <div class="container">
-    <div class="row">
-      <div class="col-sm"></div>
-      <div class="col-sm"></div>
-    </div>
-  </div>
-
-  <!-- コンテナの終わり -->
-  </div>
-
-  <!-- フッター -->
-  <footer>
-   <p>&nbsp;</p>
-   <p style="text-align:center;"><a href="#top">TOP</a></p>
-   <p>&nbsp;</p>
-   <p>&nbsp;</p>
-  </footer>
-
-  <!-- BootstrapのJS読み込み -->
-  <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-</body>
-</html>
-'''
-
-HTML_Bootstrap4H = '''<!DOCTYPE html>
-<html>
-<head>
- <meta charset="utf-8" />
- <meta http-equiv="X-UA-Compatible" content="IE=edge">
- <meta name="viewport" content="width=device-width, initial-scale=1">
- <title>(*title*)</title>
- <link rel="stylesheet" href="/css/style2.css" />
- <!-- Bootstrapの CSS 読み込み -->
- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
- <link rel="stylesheet" href="/css/style2.css">
- <!-- highlight.js -->
- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.0/styles/vs.min.css">
- <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.0/highlight.min.js"></script>
- <script>hljs.initHighlightingOnLoad();</script>
- <style></style>
- <script>
-   $(() => {
-
-   });
- </script>
-</head>
-
-<body>
-  <!-- ヘッダー -->
-  <header class="container" style="padding:24px;">
-   <h1 id="header">(*title*)</h1>
-   <h5>Updated on (*date*)</h5>
-   <hr />
-  </header>
-
-  <!-- Bootstrap コンテナ -->
-  <div class="container">
-
-    <!-- 行 -->
-    <div class="row">
-      <div class="col-sm"></div>
-      <div class="col-sm"></div>
-    </div>
-  </div>
-
-  <!-- コンテナの終わり -->
-  </div>
-
-  <!-- フッター -->
-  <footer>
-   <p>&nbsp;</p>
-   <p style="text-align:center;"><a href="#top">TOP</a></p>
-   <p>&nbsp;</p>
-   <p>&nbsp;</p>
-  </footer>
-
-  <!-- BootstrapのJS読み込み -->
-  <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-</body>
-</html>
-'''
-
-
-HTML_Vue = '''
-<!DOCTYPE html>
-<html>
-<head>
- <meta charset="utf-8" />
- <title>(*title*)</title>
- <link rel="stylesheet" href="/css/style.css" />
- <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
- <script src="/js/jquery.min.js"></script>
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
  <style>
  </style>
 </head>
 
 <body>
- <h1>(*title*)</h1>
- <div class="menubar"><a href="/">ホーム</a> / <a href="javascript:window.close()">閉じる</a></div>
- <br />
- <div id="app" class="app">
-  <div class="form_row"><label>PARAM<input type="text" v-model="value" /></label> </div>
-  <div class="form_row"><input type="button" v-on:click="submit_click" value="送信" /></div>
-  <div class="message" v-text="message"></div>
- </div>
+ <!-- ヘッダー -->
+ <header class="container">
+  <h1 class="header-1 text-primary text-center p-5">HTML_Bootstrap5</h1>
+ </header>
 
- <script>
-  const URL = "http://localhost/cgi-bin/API/testApi.cgi"
-  var app = new Vue({
-    el: "#app",
-    data: {
-        value: "",
-        message: "" 
+ <!-- 本文 -->
+ <article class="container">
+   <section class="row">
+    <div class="col p-2">
+      <p class="text-success fs-3">row1 col1</div>
+    </div>
+    <div class="col p-2">
+      <p class="text-warning fs-3 fw-bold">row1 col2</div>
+    </div>
+   </section>
+ </article>
+ 
+ <!-- フッター -->
+ <footer class="container">
+  <p class="text-center mt-4"><a href="#top">TOP</a></p>
+  <p>&nbsp;</p>
+ </footer>
+ <!-- BootstrapのJS読み込み -->
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+'''
+
+HTML_Vue3 = '''<!DOCTYPE html>
+<html lang="ja">
+<head>
+ <meta charset="utf-8" />
+ <meta name="viewport" content="width=device-width,initial-scale=1" />
+ <title>HTML_Vue3</title>
+ <!-- BootstrapのCSS読み込み -->
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+ <!-- Vue.js の読み込み -->
+ <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+ <style>
+ </style>
+</head>
+
+<body>
+  <!-- ヘッダー -->
+  <header class="container">
+   <h1 class="header-1 text-primary p-5 text-center">HTML_Vue3</h1>
+  </header>
+
+  <div id="App" class="container">
+    <div class="row p-5">
+     <div class="col">
+       <button type="button" @click="countUp" class="btn btn-primary">Count Up</button>
+     </div>
+    </div>
+    <div class="row p-5 display-6 text-danger">
+     <div class="col">
+       count = {{ count }}
+     </div>
+    </div>
+  </div>
+  
+  <!-- フッター -->
+  <footer class="container">
+   <p class="text-center mt-4"><a href="#top">TOP</a></p>
+   <p>&nbsp;</p>
+  </footer>
+  <!-- BootstrapのJS読み込み -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- Vue3 App -->
+   <script>
+    const { createApp } = Vue
+    createApp({
+      data() {
+        return { count: 0 }
       },
-    methods: {
-      submit_click: () => {
-        $.getJSON(URL, {"value":app.value}, (data) => {
-          app.message = data;
-        });
+      methods: {
+        countUp() {
+          this.count++;
+        }
       }
-    }
-  });
-</script>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
+    }).mount('#App');
+  </script>
 </body>
 </html>
 '''
@@ -246,10 +153,8 @@ else :
 
 if Common.count_args() < 2 :
   print("1: HTML5 のシンプルなページ")
-  print("2: HTML5 のシンプルなページ + jQuery + Highlight.js")
-  print("3: Bootstrap4 のページ")
-  print("4: Bootstrap4 のページ + Highlight.js ")
-  print("5: Vue.js のページ")
+  print("2: Bootstrap5 のページ")
+  print("3: Vue3 + Bootstrap5 のページ")
   pagen = int(Common.readline("番号入力 > "))
 else :
   pagen = int(Common.args(1))
@@ -259,7 +164,7 @@ if pagen < 1 or pagen > 4 :
 
 # "~" をホームディレクトリに変換する。
 filePath = fs.tilder(filePath)
-  
+
 if fs.exists(filePath) :
   Common.stop(1, filePath + " はすでに存在します。")
 
@@ -267,13 +172,20 @@ with open(filePath, "w", encoding="utf-8") as f :
   if pagen == 1 :
     f.write(HTML_Simple)
   elif pagen == 2 :
-    f.write(HTML_Highlight)
+    f.write(HTML_Bootstrap5)
   elif pagen == 3 :
-    f.write(HTML_Bootstrap4)
-  elif pagen == 4 :
-    f.write(HTML_Bootstrap4H)
+    f.write(HTML_Vue3)
   else :
-    f.write(HTML_Vue)
+    Common.stop(2, "番号が間違っています。")
 
-print("HTML ファイルが作成されました。")
+print("HTML ファイルが作成されました。必要なら下記の文を追加して下さい。")
+print(''' jQuery の追加：
+      https://code.jquery.com/jquery-3.6.3.slim.min.js
+      <script>
+        $(() => { });
+      </script>''')
+print(''' Highlight.js の追加：
+       https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/vs.min.css
+       https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js
+       hljs.initHighlightingOnLoad();''')
 
